9680ed3f9dd6d256a0e58a70cefe7c4089c1505a
Hasan Saik <hasansaik81@gmail.com>
2026-09-24 21:02:25 +0600

    login replace

--- src/app/(dashboardLayout)/layout.tsx
+++ src/app/(dashboardLayout)/layout.tsx
@@ -1,66 +1,149 @@
+// import { AppSidebar } from "@/components/layout/app-sidebar";
+// import {
+//   Breadcrumb,
+//   BreadcrumbItem,
+//   BreadcrumbLink,
+//   BreadcrumbList,
+//   BreadcrumbPage,
+//   BreadcrumbSeparator,
+// } from "@/components/ui/breadcrumb";
+// import { Separator } from "@/components/ui/separator";
+// import {
+//   SidebarInset,
+//   SidebarProvider,
+//   SidebarTrigger,
+// } from "@/components/ui/sidebar";
+// import { Roles } from "@/constants/roles";
+// import { userService } from "@/services/user.service";
+// import { redirect } from "next/navigation";
+
+
+
+// export default async function DashboardLayout({
+//   admin,
+//   user,
+// }: {
+//   children: React.ReactNode;
+//   admin: React.ReactNode;
+//   user: React.ReactNode;
+// }) {
+//   const { data } = await userService.getSession();
+
+//   if (!data?.user) {
+//     redirect("/login");
+//   }
+
+//   const userInfo = {
+//     role: data.user.role === Roles.admin ? "admin" : "user",
+//   };
+
+//   return (
+//     <SidebarProvider>
+//       <AppSidebar user={userInfo} />
+//       <SidebarInset>
+//         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+//           <SidebarTrigger className="-ml-1" />
+//           <Separator
+//             orientation="vertical"
+//             className="mr-2 data-[orientation=vertical]:h-4"
+//           />
+//           <Breadcrumb>
+//             <BreadcrumbList>
+//               <BreadcrumbItem className="hidden md:block">
+//                 <BreadcrumbLink href="#">
+//                   Building Your Application
+//                 </BreadcrumbLink>
+//               </BreadcrumbItem>
+//               <BreadcrumbSeparator className="hidden md:block" />
+//               <BreadcrumbItem>
+//                 <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+//               </BreadcrumbItem>
+//             </BreadcrumbList>
+//           </Breadcrumb>
+//         </header>
+//         <div className="flex flex-1 flex-col gap-4 p-4">
+//           {userInfo.role === "admin" ? admin : user}
+//         </div>
+//       </SidebarInset>
+//     </SidebarProvider>
+//   );
+// }
+
+
+
+
+// import { AppSidebar } from "@/components/layout/app-sidebar";
+// import {
+//   Breadcrumb,
+//   BreadcrumbItem,
+//   BreadcrumbLink,
+//   BreadcrumbList,
+//   BreadcrumbPage,
+//   BreadcrumbSeparator,
+// } from "@/components/ui/breadcrumb";
+// import { Separator } from "@/components/ui/separator";
+// import {
+//   SidebarInset,
+//   SidebarProvider,
+//   SidebarTrigger,
+// } from "@/components/ui/sidebar";
+// import { Roles } from "@/constants/roles";
+// import { userService } from "@/services/user.service";
+
+// export default async function DashboardLayout({
+//   admin,
+//   user,
+// }: {
+//   children: React.ReactNode;
+//   admin: React.ReactNode;
+//   user: React.ReactNode;
+// }) {
+//   const { data } = await userService.getSession();
+
+//   const userInfo = data.user;
+
+//   return (
+//     <SidebarProvider>
+//       <AppSidebar user={userInfo} />
+//       <SidebarInset>
+//         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+//           <SidebarTrigger className="-ml-1" />
+//         </header>
+//         <div className="flex flex-1 flex-col gap-4 p-4">
+//           {userInfo.role === Roles.admin ? admin : user}
+//         </div>
+//       </SidebarInset>
+//     </SidebarProvider>
+//   );
+// }
+
+
+
 import { AppSidebar } from "@/components/layout/app-sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Roles } from "@/constants/roles";
 import { userService } from "@/services/user.service";
-import { redirect } from "next/navigation";
+
+
 
 export default async function DashboardLayout({
-  admin,
-  user,
-}: {
-  children: React.ReactNode;
-  admin: React.ReactNode;
-  user: React.ReactNode;
-}) {
-  const { data } = await userService.getSession();
-
-  if (!data?.user) {
-    redirect("/login");
-  }
-
-  const userInfo = {
-    role: data.user.role === Roles.admin ? "admin" : "user",
-  };
-
-  return (
-    <SidebarProvider>
+  admin,user
+}:{
+  children:React.ReactNode;
+  admin:React.ReactNode;
+  user:React.ReactNode;
+}){
+  const {data}=await userService.getSession();
+  const userInfo=data.user;
+  return(
+   <SidebarProvider>
       <AppSidebar user={userInfo} />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
-          <Separator
-            orientation="vertical"
-            className="mr-2 data-[orientation=vertical]:h-4"
-          />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
-          {userInfo.role === "admin" ? admin : user}
+          {userInfo.role === Roles.admin ? admin : user}
         </div>
       </SidebarInset>
     </SidebarProvider>
